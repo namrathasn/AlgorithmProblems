7e5a341251b2fdd8cda4aceb009274a4f93b91fd
Namratha S N <73111768+namrathasn@users.noreply.github.com>
2023-01-11 20:07:20 +0530

    new

---
+++ AlgorithmProblems.java
@@ -0,0 +1,9 @@
+package com;
+
+// to print all permutations of a given string
+public class Algorithm_Problems {
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Algorith Programs");
+	}
+}
